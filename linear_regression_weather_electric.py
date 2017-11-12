6d03682aa2e8526a59d8cd48678fd6e2120cfadd
Nick Lanasa <nicklanasa@gmail.com>
2017-11-11 21:22:00 -0500

    Some more cleanup

--- linear_regression_weather_electric.py
+++ linear_regression_weather_electric.py
@@ -1,22 +1,26 @@
-from random import seed
-from random import randrange
 from utils.load_csv import load_csv
 from evaluation_metrics.rmse_metric import rmse_metric
 from utils.evaluate_algorithm import tt_evaluate_algorithm_with_metric_algorithm, evaluate_algorithm_with_metric_algorithm
 from utils.conversion_functions import int_column_to_float
 from models.linear_regression import linear_regression
-from baseline_models.zero_rule_algorithm_classification import zero_rule_algorithm_classification
 
 dataset = load_csv('datasets/electrical_output_weather.csv')
 test_dataset = load_csv('datasets/test_electrical_output_weather.csv')
 
 for i in range(len(dataset[0])):
-	int_column_to_float(dataset, i)
+    int_column_to_float(dataset, i)
 
 for i in range(len(test_dataset[0])):
-	int_column_to_float(test_dataset, i)
+    int_column_to_float(test_dataset, i)
 
-# rmse = tt_evaluate_algorithm_with_metric_algorithm(dataset, linear_regression, rmse_metric, 0.6)
-rmse = evaluate_algorithm_with_metric_algorithm(dataset, test_dataset, linear_regression, rmse_metric)
+# rmse = tt_evaluate_algorithm_with_metric_algorithm(dataset,
+#                                                    linear_regression,
+#                                                    rmse_metric,
+#                                                    0.6)
+
+rmse = evaluate_algorithm_with_metric_algorithm(dataset,
+                                                test_dataset,
+                                                linear_regression,
+                                                rmse_metric)
 
 print('RMSE: %.3f' % (rmse))
